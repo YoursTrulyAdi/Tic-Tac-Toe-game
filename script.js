e1be29a700b06081fc3fe270d874378c8fe389a0
YoursTrulyAdi <adityasahu5891@gmail.com>
2025-08-11 18:44:30 +0530

    func to input different char

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello world")
+//initializing items
+let box = document.querySelectorAll(".box");
+let latestPlay = "❌";
+
+//functions
+let playPlr = (element) =>{
+    if (latestPlay === "⭕"){
+        latestPlay = "❌";
+    }
+    else{
+        latestPlay = "⭕";
+    }
+    element.innerText = latestPlay;
+}
+
+//event listener
+box.forEach(element => {
+    element.addEventListener("click", () => {
+        playPlr(element);
+    })
+});
