59f2817f4369baecf31cad51523b8055d45c8796
YoursTrulyAdi <adityasahu5891@gmail.com>
2025-08-12 10:54:02 +0530

    Bug solved, cross plays first

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let box = document.querySelectorAll(".box");
 let btn = document.querySelector(".btn").firstElementChild;
 
-let latestPlay = "❌";
+let latestPlay = "⭕";
 let playCount = 0;
 
 let winArray = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
